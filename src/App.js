189abac9d6a12c45dfadd947986636ed30505898
TyreseSong <bowenmanuel48@gmail.com>
2023-05-07 09:37:46 +0800

    feat: add frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Initialco from './Component/initialco'
+function App() {
+  return (
+    <div className="App">
+      <Initialco />
+    </div>
+  );
+}
+
+export default App;
